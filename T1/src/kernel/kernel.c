f54472ea80030a3eda6dae8fb42afeef65571f9b
Julia <julia.zibordi@gmail.com>
2025-11-29 18:37:17 -0300

    Apaga código relativo a fifo_syscall e uma das shmems

--- T1/src/kernel/kernel.c
+++ T1/src/kernel/kernel.c
@@ -44,14 +44,13 @@ int main()
     signal(SIGINT, sigintHandler);
     signal(SIGTSTP, sigtstpHandler);
 
-    if (makeFIFO(FIFO_IRQ) == -1 || makeFIFO(FIFO_SYSCALL) == -1)
+    if (makeFIFO(FIFO_IRQ) == -1)
     {
         exit(1);
     }
 
-    int fd_irq, fd_syscall;
-    if (openFIFO(&fd_irq, FIFO_IRQ, O_RDONLY | O_NONBLOCK) == -1 ||
-        openFIFO(&fd_syscall, FIFO_SYSCALL, O_RDONLY | O_NONBLOCK) == -1)
+    int fd_irq;
+    if (openFIFO(&fd_irq, FIFO_IRQ, O_RDONLY | O_NONBLOCK) == -1)
     {
         exit(1);
     }
@@ -74,38 +73,31 @@ int main()
 
     startProcesses(processes, NUM_PROC);
 
-    shm_msg *req_shm[NUM_PROC];
-    shm_msg *rep_shm[NUM_PROC];
+    shm_msg *shm[NUM_PROC];
 
-    // Cria duas shared memories Ax - kernel: uma para requests e outra para replies
+    // Cria uma shared memory para cada processo
 
     for (int i = 0; i < NUM_PROC; i++)
     {
-        char name_req[32];
-        char name_rep[32];
+        char name_shm[32];
 
-        sprintf(name_req, "/shm_req_%d", i + 1);
-        sprintf(name_rep, "/shm_rep_%d", i + 1);
+        sprintf(name_shm, "/shm_a%d", i + 1);
 
-        int fd_req = shm_open(name_req, O_CREAT | O_RDWR, 0666);
-        int fd_rep = shm_open(name_rep, O_CREAT | O_RDWR, 0666);
-        if (fd_req < 0 || fd_rep < 0)
+        int fd = shm_open(name_shm, O_CREAT | O_RDWR, 0666);
+
+        if (fd < 0)
         {
             perror("shm_open");
             exit(1);
         }
 
-        ftruncate(fd_req, sizeof(shm_msg));
-        ftruncate(fd_rep, sizeof(shm_msg));
+        ftruncate(fd, sizeof(shm_msg));
 
-        req_shm[i] = mmap(NULL, sizeof(shm_msg), PROT_READ | PROT_WRITE, MAP_SHARED, fd_req, 0);
-        rep_shm[i] = mmap(NULL, sizeof(shm_msg), PROT_READ | PROT_WRITE, MAP_SHARED, fd_rep, 0);
+        shm[i] = mmap(NULL, sizeof(shm_msg), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
-        memset(req_shm[i], 0, sizeof(shm_msg));
-        memset(rep_shm[i], 0, sizeof(shm_msg));
+        memset(shm[i], 0, sizeof(shm_msg));
 
-        close(fd_req);
-        close(fd_rep);
+        close(fd);
     }
 
     for (int i = 0; i < NUM_PROC; i++)
@@ -136,7 +128,6 @@ int main()
 
     int current = -1;
     char irq_buf;
-    char syscall_msg[32];
 
     while (1)
     {
@@ -147,51 +138,20 @@ int main()
                           queue_D1, &n_D1, queue_D2, &n_D2);
         }
 
-        // Syscall
-        int n = read(fd_syscall, syscall_msg, sizeof(syscall_msg));
-        if (n > 0)
-        {
-            pid_t pid;
-            char dev, op;
-            int pc;
-            char state[16];
-
-            if (sscanf(syscall_msg, "%d %c %c %d %s", &pid, &dev, &op, &pc, state) == 5)
-            {
-                int idx = findProcessIndexByPid(processes, NUM_PROC, pid);
-                // Deve sempre encontrar o processo
-                if (idx == -1)
-                {
-                    printf("Erro: processo não encontrado");
-                    exit(1);
-                }
-                processes[idx].PC = pc;
-
-                if (strcmp(state, "TERMINATED") == 0)
-                {
-                    processes[idx].state = TERMINATED;
-                    kill(processes[idx].pid, SIGCONT);
-                }
-                else if (dev != '-')
-                {
-                    handleSyscallMessage(pid, dev, op, processes, NUM_PROC,
-                                         queue_D1, &n_D1, queue_D2, &n_D2);
-                }
-            }
-        }
-
         for (int i = 0; i < NUM_PROC; i++)
         {
-            if (req_shm[i]->has_request)
+            if (shm[i]->has_request)
             {
 
                 printf("[Kernel] Recebido request do processo A%d:\n", i + 1);
-                printf("op = %s\n", req_shm[i]->op);
-                printf("path = %s\n", req_shm[i]->path);
-                printf("offset = %d\n", req_shm[i]->offset);
-                printf("owner = %d\n", req_shm[i]->owner);
+                printf("op = %s\n", shm[i]->op);
+                printf("path = %s\n", shm[i]->path);
+                printf("offset = %d\n", shm[i]->offset);
+                printf("owner = %d\n", shm[i]->owner);
 
-                req_shm[i]->has_request = 0;
+                shm[i]->has_request = 0;
+                kill(processes[i].pid, SIGSTOP);
+                processes[i].state = BLOCKED;
             }
         }
 
@@ -237,10 +197,8 @@ int main()
     printProcessStates(processes, NUM_PROC);
     printf("[Kernel] - Kernel finalizando. Todos os processos acabaram sua execução\n");
     close(fd_irq);
-    close(fd_syscall);
 
     unlink(FIFO_IRQ);
-    unlink(FIFO_SYSCALL);
 
     return 0;
 }
